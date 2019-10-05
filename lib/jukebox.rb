446f12d3f8d078698cea3b1c92c4be85c32b2291
Gina Sekhar <ginasekhar@yahoo.com>
2019-10-05 18:40:37 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,13 @@
-# Add your code here
+def help
+  puts "I accept the following commands:"
+  puts " - help : displays this help message"
+  puts " - list : displayrs a list of songs to play"
+  puts " - play : lets you choose a song to play"
+  puts " - exit : exits this program"
+end
+
+def list(songs)
+  songs.each do |index|
+    puts "#{index +1}.  "
+  end
+end
